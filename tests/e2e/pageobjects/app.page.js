d7c44164f58c7ac745b7b729bab0bcec4a5e324b
Tom Kralidis <tomkralidis@gmail.com>
2021-10-20 20:01:08 -0400

    init

---
+++ tests/e2e/pageobjects/app.page.js
@@ -0,0 +1,17 @@
+class App {
+  /**
+   * elements
+   */
+  get heading() {
+    return $("h1");
+  }
+
+  /**
+   * methods
+   */
+  open(path = "/") {
+    browser.url(path);
+  }
+}
+
+module.exports = new App();
