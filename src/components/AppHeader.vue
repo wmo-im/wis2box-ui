9853a47568808147d4ef9b25502ccda9e4e8a30c
Benjamin Webb <benjamin.miller.webb@gmail.com>
2021-11-15 11:30:39 -0500

    Add header image

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,24 @@
 <template>
-  <header>
-    <a
-      href="https://community.wmo.int/activity-areas/wis/wis2-implementation"
-      title="wis2node"
+  <header :style="{ width: '100%' }">
+    <div
+      id="banner"
+      :style="{
+        'background-image': `url(${require('@/assets/img/wmo-banner.png')})`,
+      }"
     >
-      <img alt="wis2node" width="68" height="77" src="@/assets/img/logo.png" />
-    </a>
-    <h1>Welcome to WIS 2.0 node in box!</h1>
+      <a
+        href="https://community.wmo.int/activity-areas/wis/wis2-implementation"
+        title="wis2node"
+      >
+        <img
+          alt="wis2node"
+          width="68"
+          height="77"
+          src="@/assets/img/logo.png"
+        />
+      </a>
+      <br />
+      <h1>Welcome to WIS 2.0 node in box!</h1>
+    </div>
   </header>
 </template>
