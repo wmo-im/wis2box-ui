3d8276c0e034e14fa5362a6ad7129eaf67f34fc2
Benjamin Webb <40066515+webb-ben@users.noreply.github.com>
2021-12-07 13:48:30 -0500

    Update dependencies (#4)
    * Update dependency tree and run lint
    
    * Update package-lock.json

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
 };
