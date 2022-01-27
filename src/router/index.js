84c4fe76e033fa8b8d259ff6a50934323c910114
Benjamin Webb <benjamin.miller.webb@gmail.com>
2022-01-27 11:05:19 -0500

    Replace catalogue with services

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Services from "../views/Services.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/services",
+    name: "Services",
+    component: Services,
   },
 ];
 
