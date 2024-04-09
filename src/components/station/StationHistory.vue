7f8e0567f1bb5c79581607ff5f33301194898bb1
webb-ben <bwebb@lincolninst.edu>
2024-04-08 20:18:52 -0400

    Update to cartesian plotly
    - StationHistory.vue requires Histogram, update Plotly version to include while still being minified

--- src/components/station/StationHistory.vue
+++ src/components/station/StationHistory.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script>
-import Plotly from "plotly.js-basic-dist-min";
+import Plotly from "plotly.js-cartesian-dist-min";
 import { clean, hasLinks } from "@/scripts/helpers.js";
 let oapi = window.VUE_APP_OAPI;
 
