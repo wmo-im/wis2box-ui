4d34948a918aa021e56c83854ee7f87a9fc59891
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-01-10 18:42:30 -0500

    tests for language and rows on the front page

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://localhost:5173/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -71,9 +71,20 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
-    // url: 'http://127.0.0.1:5173',
-    // reuseExistingServer: !process.env.CI,
-  },
+  webServer: [
+    {
+      command: 'npm run dev',
+      url: 'http://localhost:5173',
+      stdout: 'ignore',
+      stderr: 'pipe',
+      reuseExistingServer: !process.env.CI,
+    },
+    {
+      command: 'npm run start-backend-proxy',
+      url: "http://localhost:8010/proxy",
+      stdout: 'ignore',
+      stderr: 'pipe',
+      reuseExistingServer: !process.env.CI,
+    }, 
+  ]
 });
