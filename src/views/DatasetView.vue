1bbc106540135da806c01690300297ecca4da773
Benjamin Webb <40066515+webb-ben@users.noreply.github.com>
2025-01-30 07:52:54 -0700

    Update DatasetView.vue

--- src/views/DatasetView.vue
+++ src/views/DatasetView.vue
@@ -14,7 +14,7 @@
         <v-col sm="12" md="3">
           <v-container>
             <v-row justify="center" fill-height>
-              <template v-if="dataset.hasObs">
+              <template v-if="dataset.hasSynop">
                 <v-card class="pa-0 ma-0" @click="loadMap(dataset.id)">
                   <v-overlay open-on-hover contained activator="parent" class="align-center justify-center">
                     <v-btn flat>
