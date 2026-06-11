8eea2f994d1715812eb31ed09434cf727390f6a0
Maaike <maaike.limper@gmail.com>
2026-06-10 21:03:32 -0400

    update fetchMessages method for api call

--- src/components/TempChartDialog.vue
+++ src/components/TempChartDialog.vue
@@ -74,9 +74,9 @@ export default defineComponent({
       this.loading = true;
       try {
         const url = `${window.VUE_APP_OAPI}/collections/messages/items?` + new URLSearchParams({
-          metadata_id: this.metadata_id,
-          q: "canonical",
           wigos_station_identifier: this.selectedStation.id,
+          data_id: this.metadata_id.replace(/^urn:wmo:md:?/, ""),
+          q: "canonical",
           limit: "10",
           sortby: "-datetime"
         });
